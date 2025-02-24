de086559bd15284a842304d85890d68978f3990c
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-02-24 21:53:12 +0100

    add better escape

---
+++ config/plugins/better-escape/default.nix
@@ -0,0 +1,27 @@
+{
+  plugins = {
+    better-escape = {
+      enable = true;
+      settings = {
+        default_mappings = false;
+        mappings = {
+          c = {
+            j = {
+              j = "<Esc>";
+            };
+          };
+          i = {
+            j = {
+              j = "<Esc>";
+            };
+          };
+          s = {
+            j = {
+              j = "<Esc>";
+            };
+          };
+        };
+      };
+    };
+  };
+}
