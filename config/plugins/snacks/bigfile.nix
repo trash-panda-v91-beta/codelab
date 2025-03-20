971d853c1e39512003a68ff10dce8a6f5b81132f
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-03-20 20:10:00 +0100

    (snacks) add bigfile snack

---
+++ config/plugins/snacks/bigfile.nix
@@ -0,0 +1,8 @@
+_: {
+  plugins.snacks = {
+    settings = {
+      bigfile = {
+      };
+    };
+  };
+}
--- config/plugins/snacks/default.nix
+++ config/plugins/snacks/default.nix
@@ -7,6 +7,7 @@
     };
   };
   imports = [
+    ./bigfile.nix
     ./dashboard.nix
     ./git.nix
     ./indent.nix
