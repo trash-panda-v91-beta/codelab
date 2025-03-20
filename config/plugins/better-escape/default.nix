522e91bfb84eb293120a89afc3a90c0e93ab0805
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-03-20 22:04:56 +0100

    (better-escape) enable only in insert mode

--- config/plugins/better-escape/default.nix
+++ config/plugins/better-escape/default.nix
@@ -2,6 +2,17 @@
   plugins = {
     better-escape = {
       enable = true;
+      settings = {
+        default_mappings = false;
+        mappings = {
+          i = {
+            j = {
+              j = "<Esc>";
+              k = "<Esc>";
+            };
+          };
+        };
+      };
     };
   };
 }
