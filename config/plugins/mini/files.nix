030f7acf6a6ed8d3732a74b8d0890b73e6de7f7b
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-03-22 18:56:22 +0100

    (mini) remove filter for files

--- config/plugins/mini/files.nix
+++ config/plugins/mini/files.nix
@@ -2,12 +2,6 @@
 
   plugins.mini.modules.files = {
 
-    content = {
-      filter.__raw = ''
-        function(entry)
-          return entry.name ~= '.git' and entry.name ~= '.direnv' and entry.name ~= '.envrc' and entry.name ~= '.gitlab' and entry.name ~= '.github'
-        end'';
-    };
     windows = {
       preview = true;
       width_preview = 100;
