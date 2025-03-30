61336034f5f0aa7a1581c603846c9ba01e8e712f
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-03-30 19:20:05 +0200

    (codecompanion) refactor lualine component

--- config/plugins/codecompanion/default.nix
+++ config/plugins/codecompanion/default.nix
@@ -1,3 +1,58 @@
+let
+  lualine_component = ''
+    (function()
+         local M = require("lualine.component"):extend()
+
+         M.processing = false
+         M.spinner_index = 1
+
+         local spinner_symbols = {
+           "⠋",
+           "⠙",
+           "⠹",
+           "⠸",
+           "⠼",
+           "⠴",
+           "⠦",
+           "⠧",
+           "⠇",
+           "⠏",
+         }
+         local spinner_symbols_len = 10
+
+         -- Initializer
+         function M:init(options)
+           M.super.init(self, options)
+
+           local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+           vim.api.nvim_create_autocmd({ "User" }, {
+             pattern = "CodeCompanionRequest*",
+             group = group,
+             callback = function(request)
+               if request.match == "CodeCompanionRequestStarted" then
+                 self.processing = true
+               elseif request.match == "CodeCompanionRequestFinished" then
+                 self.processing = false
+               end
+             end,
+           })
+         end
+
+         -- Function that runs every time statusline is updated
+         function M:update_status()
+           if self.processing then
+             self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+             return spinner_symbols[self.spinner_index]
+           else
+             return nil
+           end
+         end
+
+         return M
+    end)()
+  '';
+in
 {
   plugins = {
     codecompanion = {
@@ -50,59 +105,7 @@
     lualine.settings = {
       sections.lualine_x = [
         {
-          __unkeyed.__raw = ''
-            (function()
-               local M = require("lualine.component"):extend()
-
-               M.processing = false
-               M.spinner_index = 1
-
-               local spinner_symbols = {
-                 "⠋",
-                 "⠙",
-                 "⠹",
-                 "⠸",
-                 "⠼",
-                 "⠴",
-                 "⠦",
-                 "⠧",
-                 "⠇",
-                 "⠏",
-               }
-               local spinner_symbols_len = 10
-
-               -- Initializer
-               function M:init(options)
-                 M.super.init(self, options)
-
-                 local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
-
-                 vim.api.nvim_create_autocmd({ "User" }, {
-                   pattern = "CodeCompanionRequest*",
-                   group = group,
-                   callback = function(request)
-                     if request.match == "CodeCompanionRequestStarted" then
-                       self.processing = true
-                     elseif request.match == "CodeCompanionRequestFinished" then
-                       self.processing = false
-                     end
-                   end,
-                 })
-               end
-
-               -- Function that runs every time statusline is updated
-               function M:update_status()
-                 if self.processing then
-                   self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
-                   return spinner_symbols[self.spinner_index]
-                 else
-                   return nil
-                 end
-               end
-
-               return M
-             end)()
-          '';
+          __unkeyed.__raw = lualine_component;
         }
       ];
       extensions = [
@@ -144,6 +147,7 @@
               '';
             };
             lualine_x = {
+              __unkeyed.__raw = lualine_component;
             };
             lualine_y = {
               __unkeyed = "progress";
