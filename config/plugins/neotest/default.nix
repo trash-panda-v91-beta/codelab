97eb11add1e61a9c59efd9ffe0845a95ca376526
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-10 13:06:02 +0200

    (neotest) add keymaps

--- config/plugins/neotest/default.nix
+++ config/plugins/neotest/default.nix
@@ -7,4 +7,51 @@
       };
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>tf";
+      action = "<cmd>Neotest run file<CR>";
+      options = {
+        silent = true;
+        desc = "Run test for a file";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tt";
+      action = "<cmd>Neotest run<CR>";
+      options = {
+        silent = true;
+        desc = "Run test for a function";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tl";
+      action = "<cmd>Neotest run last<CR>";
+      options = {
+        silent = true;
+        desc = "Run last test";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ts";
+      action = "<cmd>Neotest summary<CR>";
+      options = {
+        silent = true;
+        desc = "Show test summary";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>to";
+      action = "<cmd>Neotest output-panel<CR>";
+      options = {
+        silent = true;
+        desc = "Show test output";
+      };
+    }
+  ];
 }
