eadcee7eb6c2ccb24a400f88939d36d4d22c483a
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-12 01:22:39 +0200

    (cyberdream) add more config options

--- config/plugins/cyberdream/default.nix
+++ config/plugins/cyberdream/default.nix
@@ -3,6 +3,8 @@
     enable = true;
     lazyLoad.settings.colorscheme = "cyberdream";
     settings = {
+      borderless_picker = true;
+      saturation = 1;
       italic_comments = true;
       hide_fillchars = true;
       transparent = true;
