61383bd5d3fd88d6d7976275f8e66d8868726894
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-12 00:15:33 +0200

    (avante) add

---
+++ config/plugins/avante/default.nix
@@ -0,0 +1,47 @@
+{
+  plugins.avante = {
+    enable = false;
+
+    lazyLoad.settings.event = [ "BufEnter" ];
+    settings = {
+      mappings = {
+        files = {
+          add_current = "<leader>a.";
+        };
+      };
+      diff = {
+        autojump = true;
+        debug = false;
+        list_opener = "copen";
+      };
+      highlights = {
+        diff = {
+          current = "DiffText";
+          incoming = "DiffAdd";
+        };
+      };
+      hints = {
+        enabled = true;
+      };
+      mappings = {
+        diff = {
+          both = "cb";
+          next = "]x";
+          none = "c0";
+          ours = "co";
+          prev = "[x";
+          theirs = "tc";
+        };
+      };
+      provider = "copilot";
+      windows = {
+        sidebar_header = {
+          align = "center";
+          rounded = true;
+        };
+        width = 30;
+        wrap = true;
+      };
+    };
+  };
+}
