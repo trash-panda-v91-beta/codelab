d2276d181304cf155ad0bc8a392445ebafb901ee
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-11 23:03:22 +0200

    (markview) customize

--- config/plugins/markview/default.nix
+++ config/plugins/markview/default.nix
@@ -1,25 +1,65 @@
 {
-  plugins.markview = {
-    enable = true;
-    settings = {
-      preview = {
-        buf_ignore = [ ];
-        callbacks.on_enable.__raw = ''
-          function(_, win)
-            vim.wo[win].conceallevel = 2
-            vim.wo[win].concealcursor = "nc"
-          end
-        '';
-        hybrid_modes = [
-          "i"
-        ];
-        modes = [
-          "n"
-          "i"
-          "no"
-          "c"
-        ];
+  plugins = {
+    markview = {
+      enable = true;
+      luaConfig.post = ''
+        require("markview.extras.checkboxes").setup({
+            default = "X",
+            remove_style = "disable",
+            states = {
+                { " ", "/", "X" },
+                { "<", ">" },
+                { "?", "!", "*" },
+                { '"' },
+                { "l", "b", "i" },
+                { "S", "I" },
+                { "p", "c" },
+                { "f", "k", "w" },
+                { "u", "d" }
+            }
+        })
+      '';
+
+      lazyLoad.enable = false; # NOTE: it is already lazy-loaded
+      settings = {
+        html.enable = true;
+        latex.enable = true;
+
+        preview = {
+          callbacks.on_enable.__raw = ''
+            function(_, win)
+              vim.wo[win].conceallevel = 2
+              vim.wo[win].concealcursor = "nc"
+              vim.wo[win].wrap = false
+            end
+          '';
+          hybrid_modes = [
+            "i"
+          ];
+          modes = [
+            "n"
+            "i"
+            "nc"
+            "c"
+          ];
+        };
       };
     };
+    blink-cmp = {
+      settings.sources.per_filetype.markdown = [ "markview" ];
+    };
   };
+  keymaps = [
+    {
+      mode = [
+        "n"
+        "i"
+      ];
+      key = "<M-d>";
+      action = "<cmd>Checkbox toggle<cr>";
+      options = {
+        desc = "Toggle Checkbox";
+      };
+    }
+  ];
 }
