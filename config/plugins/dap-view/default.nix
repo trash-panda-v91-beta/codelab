3e521c540015f77904588a92fe7a9e3bf6c3db8c
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-04-24 13:55:58 +0200

    feat(debug): integrate dap-view and dap-virtual-text plugins

---
+++ config/plugins/dap-view/default.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.plugins.nvim-dap-view.enable = lib.mkEnableOption "nvim-dap-view" // {
+    default = !config.plugins.dap-ui.enable;
+  };
+
+  config = lib.mkIf config.plugins.nvim-dap-view.enable {
+    extraPlugins = [
+      pkgs.vimPlugins.nvim-dap-view
+    ];
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>du";
+        action.__raw = ''
+          function()
+            require('dap.ext.vscode').load_launchjs(nil, {})
+            require("dap-view").toggle()
+          end
+        '';
+        options = {
+          desc = "Toggle Debugger UI";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>dw";
+        action = "<CMD>DapViewWatch<CR>";
+        options = {
+          desc = "Add Watch";
+        };
+      }
+    ];
+  };
+}
---
+++ config/plugins/dap-virtual-text/default.nix
@@ -0,0 +1,20 @@
+{
+  plugins = {
+    dap-virtual-text = {
+      enable = true;
+
+      lazyLoad.settings = {
+        before.__raw = ''
+          function()
+            require('lz.n').trigger_load('nvim-dap')
+          end
+        '';
+        cmd = [
+          "DapVirtualTextEnable"
+          "DapVirtualTextForceRefresh"
+          "DapVirtualTextToggle"
+        ];
+      };
+    };
+  };
+}
