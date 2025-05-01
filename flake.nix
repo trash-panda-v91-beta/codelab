846c9e79cb0664ad7fab6afffe879ee11a091f6f
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-05-01 09:50:06 +0200

    configure treefmt

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
 
     snacks-nvim = {
       url = "github:folke/snacks.nvim";
@@ -12,7 +13,13 @@
   };
 
   outputs =
-    { nixpkgs, nixvim, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      nixvim,
+      treefmt-nix,
+      ...
+    }@inputs:
     let
       systems = [
         "x86_64-linux"
@@ -21,6 +28,9 @@
         "aarch64-darwin"
       ];
       forAllSystems = fn: nixpkgs.lib.genAttrs systems fn;
+      treefmtEval = forAllSystems (
+        system: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} ./treefmt.nix
+      );
     in
     {
       packages = forAllSystems (
@@ -58,19 +68,8 @@
         }
       );
 
-      checks = forAllSystems (
-        system:
-        let
-          nixvimLib = nixvim.lib.${system};
-          nixvimModule = {
-            inherit system;
-            module = import ./config;
-            extraSpecialArgs = { inherit inputs; };
-          };
-        in
-        {
-          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-        }
-      );
+      checks = forAllSystems (system: {
+        formatting = treefmtEval.${system}.config.build.check self;
+      });
     };
 }
