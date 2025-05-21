83fe80f24f92037463fda7ef3520362bdff85c48
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2025-05-21 10:52:18 +0200

    Add `fastaction` plugin

---
+++ config/plugins/fastaction/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }:
+{
+  plugins = {
+    fastaction = {
+      enable = true;
+    };
+
+  };
+  keymaps = lib.mkIf config.plugins.fastaction.enable [
+    {
+      mode = [
+        "n"
+        "i"
+      ];
+      key = "<leader>ca";
+      action = "<cmd>lua require('fastaction').code_action()<CR>";
+      options = {
+        desc = "Display code actions";
+        buffer.__raw = "bufnr";
+      };
+    }
+  ];
+}
