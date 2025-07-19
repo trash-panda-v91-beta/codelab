b9bfd5e34c66d1b613747e1c75e7746b71a0fd42
trash-panda-v91-beta <42897550+trash-panda-v91-beta@users.noreply.github.com>
2025-07-19 11:37:45 +0200

    fix(kulala): fix treesitter

--- config/plugins/kulala/default.nix
+++ config/plugins/kulala/default.nix
@@ -1,4 +1,22 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  treesitter-kulala-http-grammar = pkgs.tree-sitter.buildGrammar {
+    language = "kulala_http";
+    version = "5.3.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "mistweaverco";
+      repo = "kulala.nvim";
+      rev = "902fc21e8a3fee7ccace37784879327baa6d1ece";
+      hash = "sha256-whQpwZMEvD62lgCrnNryrEvfSwLJJ+IqVCywTq78Vf8=";
+    };
+    location = "lua/tree-sitter";
+  };
+in
 {
   plugins = {
     kulala = {
@@ -12,9 +30,24 @@
         };
       };
     };
+    treesitter = lib.mkIf config.plugins.kulala.enable {
+      grammarPackages = [ treesitter-kulala-http-grammar ];
+      luaConfig.post = # lua
+        ''
+          do
+          	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+          	parser_config.kulala_http = {
+          		install_info = {
+          			url = "${treesitter-kulala-http-grammar}",
+          			files = { "src/parser.c" },
+          		},
+          		filetype = "kulala_http",
+          	}
+          end
+        '';
+    };
   };
 
   keymaps = lib.mkIf config.plugins.kulala.enable [
-
   ];
 }
